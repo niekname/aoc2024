ec02179ef518710273a4a3fed8a0037bb5fc5ea0
Niek Vervaeke <niek.vervaeke@gmail.com>
2024-12-03 23:04:10 +0100

    day 3 part 2

--- src/main/kotlin/be/niekname/aoc2024/Day3.kt
+++ src/main/kotlin/be/niekname/aoc2024/Day3.kt
@@ -14,3 +14,27 @@ class Day3Part1 {
             .map(String::toInt)
             .reduce { left, right -> left * right }
 }
+
+class Day3Part2 {
+    fun fixCorruptedMemory(input: String): Int {
+        val regex = "mul\\(\\d{1,3},\\d{1,3}\\)|don't\\(\\)|do\\(\\)".toRegex()
+        val instructions = regex.findAll(input).map { it.value }
+
+        var mulEnabled = true
+        var result = 0
+        instructions.forEach {
+            if (it == "do()") mulEnabled = true
+            else if (it == "don't()") mulEnabled = false
+            else if (mulEnabled) result += doMultiply(it)
+        }
+
+        return result
+    }
+
+    private fun doMultiply(mul: String) =
+        mul.removePrefix("mul(")
+            .removeSuffix(")")
+            .split(",")
+            .map(String::toInt)
+            .reduce { left, right -> left * right }
+}
--- src/test/kotlin/be/niekname/aoc2024/Day3Test.kt
+++ src/test/kotlin/be/niekname/aoc2024/Day3Test.kt
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test
 
 class Day3Test {
 
-    val exampleInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
     @Nested
     inner class Part1Test {
+        private val exampleInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
         @Test
         fun `fix memory`() {
             val result = Day3Part1().fixCorruptedMemory(exampleInput)
@@ -17,4 +18,14 @@ class Day3Test {
         }
     }
 
+    @Nested
+    inner class Part2Test {
+        private val exampleInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+        @Test
+        fun `fix memory`() {
+            val result = Day3Part2().fixCorruptedMemory(exampleInput)
+            assertThat(result).isEqualTo(48)
+        }
+    }
 }
